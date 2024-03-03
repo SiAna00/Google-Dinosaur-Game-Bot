e5e90299648ab610e7da146ff7a01f39a780a753
Ana <asimicev@gmail.com>
2024-03-03 17:16:54 +0100

    automate opening dinosaur game website, automate jumping when encountering obstacles

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import pyautogui
+import time
+
+time.sleep(3)
+
+pyautogui.moveTo(670, 1045)
+pyautogui.click()
+
+time.sleep(3)
+
+pyautogui.write("https://elgoog.im/t-rex/")
+pyautogui.hotkey("enter")
+
+time.sleep(10)
+
+pyautogui.hotkey("space")
+
+while True:
+    if pyautogui.pixel(420, 790)[0] == 83 or pyautogui.pixel(420, 710)[0] == 83:
+        pyautogui.hotkey("space")
